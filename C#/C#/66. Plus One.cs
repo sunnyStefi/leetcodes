7cd879aa6eaeb3e95a5f7cfbaa23e94b35011138
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2024-05-13 15:48:12 +0200

    plus one

---
+++ C#/C#/66. Plus One.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.PortableExecutable;
+
+public class PlusOneClass {
+
+    //{9,9,9,9,9,9,9,9,9,9,9};
+    //{1,2,3,4}
+    //{1,2,3,9}
+    //{1,2,9,9}
+    public int[] PlusOne(int[] digits) {
+       bool increaseNextLevel = false;
+       List<int> tempArray = new List<int>(); // increase when there is a chain of 9s til the end
+       for(int i = digits.Length -1; i >= 0; i--){
+        if (digits[i] != 9 && !increaseNextLevel ){
+            tempArray.Add(digits[i]+1);
+            increaseNextLevel = true;
+            continue;
+        }
+        if (digits[i] == 9 && !increaseNextLevel){
+            tempArray.Add(0);
+            increaseNextLevel = false;
+            continue;
+        }
+        tempArray.Add(digits[i]);
+       }
+       if (!increaseNextLevel) { //was 999..
+        tempArray.Add(1);
+       }
+       tempArray.Reverse();
+        return tempArray.ToArray();
+    }
+}
--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -1,14 +1,27 @@
 public class Program {
     public static void Main(string[] args) 
     {
-        int [] inputArray = {1,2,4,5};
-        SearchInsertClass searchInsertClass = new SearchInsertClass();
-        int res = searchInsertClass.SearchInsert(inputArray, 7);
-        Console.WriteLine(res);
+        // //35
+        // int [] inputArray = {1,2,4,5};
+        // SearchInsertClass searchInsertClass = new SearchInsertClass();
+        // int res = searchInsertClass.SearchInsert(inputArray, 7);
+        // Console.WriteLine("35:" + res);
         
-        string input = "asd 123456 ";
-        LengthOfLastWordClass lengthOfLastWordClass = new LengthOfLastWordClass();
-        res = lengthOfLastWordClass.LengthOfLastWord(input);
-        Console.WriteLine(res);
+        // //58
+        // string input = "asd 123456 ";
+        // LengthOfLastWordClass lengthOfLastWordClass = new LengthOfLastWordClass();
+        // res = lengthOfLastWordClass.LengthOfLastWord(input);
+        // Console.WriteLine("58:" + res);
+
+         //66
+        //  int [] plusOneClassInput = new int[]{1,2,3,4}; 
+        // int [] plusOneClassInput = new int[]{1,2,3,9}; 
+        //  int [] plusOneClassInput = new int[]{1,2,9,9}; 
+        //  int [] plusOneClassInput = new int[]{9,9,9,9}; 
+           int [] plusOneClassInput = new int[]{9,8,7}; 
+        //  int [] plusOneClassInput = new int[]{9,9,9,9,9,9,9,9,9,9,9};
+        PlusOneClass plusOneClass = new PlusOneClass();
+        int [] resArray = plusOneClass.PlusOne(plusOneClassInput);
+        Console.WriteLine("66:" + string.Join(",", resArray));
     }
 }
